7b5e5803d41fb9b477beb0d3cf676e59ff2a8280
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-03-27 11:40:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function delayedHello() {
+			return new Promise((resolve) => {
+				setTimeout(() => {
+					resolve('Hello, world!');
+				}, 1000);
+			});
+		}
+
+		delayedHello().then((message) => {
+			const outputElement = document.getElementById('output');
+			outputElement.textContent = message;
+		});
